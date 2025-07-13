25f25fee0451766fb4a841561f5f0dced9623b0a
Shreya Jaiswal <2023eeb1245@iitrpr.ac.in>
2025-07-13 12:29:01 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBqcvCOgNg_I7eXYGregJR63dMW0Ol7yTs",
-  authDomain: "fit-track-6baa1.firebaseapp.com",
-  projectId: "fit-track-6baa1",
-  storageBucket: "fit-track-6baa1.firebasestorage.app",
-  messagingSenderId: "127123716921",
-  appId: "1:127123716921:web:145117928ad749356f06e8",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
